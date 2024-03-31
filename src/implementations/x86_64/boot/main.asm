5c2afea2e6e5e07206b74b9cef1cfbab5852a7b6
Deonn Almirol <deonn.almirol@gmail.com>
2024-03-30 18:54:23 -0700

    real

--- src/implementations/x86_64/boot/main.asm
+++ src/implementations/x86_64/boot/main.asm
@@ -4,8 +4,10 @@ extern long_mode_start
 section .text
 bits 32
 start:
+	; move address of stack_top into esp register
 	mov esp, stack_top
 
+	; call subroutines that check for 64-bit support
 	call check_multiboot
 	call check_cpuid
 	call check_long_mode
@@ -16,9 +18,12 @@ start:
 	lgdt [gdt64.pointer]
 	jmp gdt64.code_segment:long_mode_start
 
+	;print 'OK' by writing directly to 'video memory' which begins at 0xb8000
+	mov dword [0xb8000], 0x2f4fa45d
 	hlt
 
 check_multiboot:
+	; check if this magic number is in eax register
 	cmp eax, 0x36d76289
 	jne .no_multiboot
 	ret
@@ -54,7 +59,6 @@ check_long_mode:
 	cpuid
 	test edx, 1 << 29
 	jz .no_long_mode
-	
 	ret
 .no_long_mode:
 	mov al, "L"
@@ -62,25 +66,25 @@ check_long_mode:
 
 setup_page_tables:
 	mov eax, page_table_l3
-	or eax, 0b11 ; present, writable
+	or eax, 0b11 ; enable present, writable flags
 	mov [page_table_l4], eax
-	
+
 	mov eax, page_table_l2
-	or eax, 0b11 ; present, writable
+	or eax, 0b11 ; enable present, writable flags
 	mov [page_table_l3], eax
 
 	mov ecx, 0 ; counter
-.loop:
+
+.loop: 
 
 	mov eax, 0x200000 ; 2MiB
 	mul ecx
 	or eax, 0b10000011 ; present, writable, huge page
-	mov [page_table_l2 + ecx * 8], eax
+	mov [page_table_l2 + ecx * 8], eax 
 
 	inc ecx ; increment counter
-	cmp ecx, 512 ; checks if the whole table is mapped
+	cmp ecx, 512 ; checks if whole table is mapped
 	jne .loop ; if not, continue
-
 	ret
 
 enable_paging:
@@ -88,7 +92,7 @@ enable_paging:
 	mov eax, page_table_l4
 	mov cr3, eax
 
-	; enable PAE
+	; enable physical address extension
 	mov eax, cr4
 	or eax, 1 << 5
 	mov cr4, eax
@@ -103,26 +107,27 @@ enable_paging:
 	mov eax, cr0
 	or eax, 1 << 31
 	mov cr0, eax
-
 	ret
 
 error:
-	; print "ERR: X" where X is the error code
+	; print 'err: x' where x is the error code
 	mov dword [0xb8000], 0x4f524f45
 	mov dword [0xb8004], 0x4f3a4f52
 	mov dword [0xb8008], 0x4f204f20
 	mov byte  [0xb800a], al
 	hlt
 
+
 section .bss
 align 4096
 page_table_l4:
-	resb 4096
-page_table_l3:
-	resb 4096
-page_table_l2:
-	resb 4096
+ resb 4096
+ page_table_l3:
+ resb 4096
+ page_table_l2:
+ resb 4096
 stack_bottom:
+	; reserve 16kb of memory
 	resb 4096 * 4
 stack_top:
 
@@ -130,7 +135,7 @@ section .rodata
 gdt64:
 	dq 0 ; zero entry
 .code_segment: equ $ - gdt64
-	dq (1 << 43) | (1 << 44) | (1 << 47) | (1 << 53) ; code segment
+	dq (1 << 43) | (1 << 44) | (1 << 47) | (1 << 53)  ; code segment
 .pointer:
-	dw $ - gdt64 - 1 ; length
-	dq gdt64 ; address
+	dw $ - gdt64 - 1
+	dq gdt64
